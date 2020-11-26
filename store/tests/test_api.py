0d78f4bb1f972b443a2801e4746f153406796c25
Muhammad Israpilov <42israpilov@mail.ru>
2020-11-26 14:11:36 +0300

    Added permissions update tests views models

--- store/tests/test_api.py
+++ store/tests/test_api.py
@@ -3,6 +3,7 @@ import json
 from django.urls import reverse
 from rest_framework import status
 from django.contrib.auth.models import User
+from rest_framework.exceptions import ErrorDetail
 from rest_framework.test import APITestCase
 
 from store.models import Book
@@ -12,9 +13,9 @@ from store.serializers import BooksSerializer
 class BooksApiTestCase(APITestCase):
     def setUp(self):
         self.user = User.objects.create(username='test username')
-        self.book1 = Book.objects.create(name='Test book 1', price=100, author_name='author1')
-        self.book2 = Book.objects.create(name='Test book 2', price=200, author_name='author2')
-        self.book3 = Book.objects.create(name='Test book 3 author1', price=200, author_name='author3')
+        self.book1 = Book.objects.create(name='Test book 1', price=100, author_name='author1', owner=self.user)
+        self.book2 = Book.objects.create(name='Test book 2', price=200, author_name='author2', owner=self.user)
+        self.book3 = Book.objects.create(name='Test book 3 author1', price=200, author_name='author3', owner=self.user)
 
     def test_get(self):
         url = reverse('book-list')
@@ -57,6 +58,7 @@ class BooksApiTestCase(APITestCase):
         response = self.client.post(url, data=json_data, content_type='application/json')
         self.assertEqual(status.HTTP_201_CREATED, response.status_code)
         self.assertEqual(4, Book.objects.all().count())
+        self.assertEqual(self.user, Book.objects.last().owner)
 
     def test_update(self):
         url = reverse('book-detail', args=(self.book1.id,))
@@ -79,3 +81,38 @@ class BooksApiTestCase(APITestCase):
         response = self.client.delete(url)
         self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
         self.assertEqual(2, Book.objects.all().count())
+
+    def test_update_not_owner(self):
+        self.user2 = User.objects.create(username='test username2')
+        url = reverse('book-detail', args=(self.book1.id,))
+        data = {
+            "name": self.book1.name,
+            "price": "4000.00",
+            "author_name": self.book1.author_name,
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user2)
+        response = self.client.put(url, data=json_data, content_type='application/json')
+        self.assertEqual(status.HTTP_403_FORBIDDEN, response.status_code)
+        self.assertEqual({'detail': ErrorDetail(string='You do not have permission to perform this action.',
+                                                code='permission_denied')}, response.data)
+        self.book1.refresh_from_db()
+        self.assertEqual(100, self.book1.price)
+
+    def test_update_not_owner_but_staff(self):
+        self.user2 = User.objects.create(username='test username2', is_staff=True)
+        url = reverse('book-detail', args=(self.book1.id,))
+        data = {
+            'id': self.book1.id,
+            'name': self.book1.name,
+            'price': '4000.00',
+            'author_name': self.book1.author_name,
+            'owner': self.book1.owner.id
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user2)
+        response = self.client.put(url, data=json_data, content_type='application/json')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.book1.refresh_from_db()
+        self.assertEqual(data, response.data)
+        self.assertEqual(4000.00, self.book1.price)
--- store/tests/test_serializer.py
+++ store/tests/test_serializer.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 from store.models import Book
 from store.serializers import BooksSerializer
@@ -5,22 +6,25 @@ from store.serializers import BooksSerializer
 
 class BookSerializerTestCase(TestCase):
     def test_ok(self):
+        # user = User.objects.create(username='test_username')
         book1 = Book.objects.create(name='Test book 1', price='100.00', author_name='Mark 1')
         book2 = Book.objects.create(name='Test book 2', price='200.00', author_name='Mark 2')
         data = BooksSerializer([book1, book2], many=True).data
         expected_data = [
             {
-                'id': book1 .id,
+                'id': book1.id,
                 'name': book1.name,
                 'price': book1.price,
-                'author_name' : book1.author_name
+                'author_name': book1.author_name,
+                'owner': book1.owner
             },
 
             {
                 'id': book2.id,
                 'name': book2.name,
                 'price': book2.price,
-                'author_name': book2.author_name
+                'author_name': book2.author_name,
+                'owner': book2.owner
             }
         ]
         self.assertEqual(expected_data, data)
